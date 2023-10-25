5f8634ee728edd002b3ef39837b8dddbbf65b7de
Maidana Ivan <ivan.maidana99122@gmail.com>
2023-10-25 00:05:02 -0300

    first commit

--- front/src/app/NavBar.jsx
+++ front/src/app/NavBar.jsx
@@ -1,5 +1,8 @@
+import Image from 'next/image'
 import Carrito from '../../public/shopping_cart.svg'
+import logo from '../../public/logo.png'
 import Link from 'next/link'
+
 
 export default function NavBar() {
     return (
@@ -11,13 +14,16 @@ export default function NavBar() {
                 </ul>
             </nav>
             <nav className="text-black bg-yellow-50 py-6 px-10 flex items-center fixed top-10 w-full justify-between">
-                <h1 className="flex flex-grow basic-0 font-extrabold text-lg">Sixteen</h1>
-                <ul className="flex flex-grow justify-center basic-0 text-sm [&>li>a]:inline-block [&>li>a]:px-4 [&>li>a]:py-3 [&>li>a]:text-black">
+                <div className='flex flex-grow basis-0 justify-items-start'>
+                    <Image className='basis-0' width={100} height={50} src={logo} alt='logo' />
+                    <span className="font-extrabold text-lg basis-0">Sixteen</span>
+                </div>   
+                <ul className="flex flex-grow justify-center basis-0 text-sm [&>li>a]:inline-block [&>li>a]:px-4 [&>li>a]:py-3 [&>li>a]:text-black">
                     <li><Link href="/">Home</Link></li>
                     <li><Link href="/about-us">About Us</Link></li>
-                    <li><Link>Contact Us</Link></li>
+                    <li><Link href="/contact-us">Contact Us</Link></li>
                 </ul>
-                <ul className="flex flex-grow justify-end basic-0 text-sm [&>li>a]:inline-block [&>li>a]:px-4 [&>li>a]:py-3 [&>li>a]:text-black">
+                <ul className="flex flex-grow justify-end basis-0 text-sm [&>li>a]:inline-block [&>li>a]:px-4 [&>li>a]:py-3 [&>li>a]:text-black">
                     <li><Link href="/"><Carrito className="w-5 h-5"/></Link></li>
                     <li><Link href="/">User</Link></li>
                 </ul>
