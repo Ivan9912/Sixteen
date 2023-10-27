cd37a707a5524750e1dc96b3fa90d4f2edf6d7d3
Maidana Ivan <ivan.maidana99122@gmail.com>
2023-10-27 03:52:24 -0300

    first commit

--- front/src/app/page.jsx
+++ front/src/app/page.jsx
@@ -1,10 +1,10 @@
 export default function Home() {
     return (
         <main className="flex  text-black flex-col items-center justify-between pt-96 pb-96">
-            <blockquote class="text-2xl font-semibold italic text-center text-slate-900">
+            <blockquote className="text-2xl font-semibold italic text-center text-slate-900">
                 Bienvenidos a&nbsp;
-                <span class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
-                    <span class="relative text-white">Sixteen</span>
+                <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
+                    <span className="relative text-white">Sixteen</span>
                 </span>
                 !
             </blockquote>
--- front/src/components/Main-Nav/NavBar.jsx
+++ front/src/components/Main-Nav/NavBar.jsx
@@ -10,9 +10,6 @@ import { useState } from 'react';
 export default function NavBar() {
     const [isOpenMenu, setIsOpenMenu] = useState(false);
 
-    const toggleMenuHamburg = () => {
-        setIsOpenMenu(!isOpenMenu);
-    };
     return (
         <header className='divide-y-2 divide-y-reverse divide-slate-400/25'>
             <nav className="w-screen h-10 text-slate-50 bg-stone-700 py-4 px-10 md:flex items-center fixed md:top-0 justify-between align-middle hidden">
@@ -23,11 +20,11 @@ export default function NavBar() {
             </nav>
             <nav className="text-black bg-white py-2 px-10 flex items-center fixed top-0 md:top-10 w-screen justify-between">
                 <div className='flex flex-grow basis-0 justify-items-start'>
-                    <Link href="/" className="font-extrabold text-lg basis-0 flex items-center justify-center "><span class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 hover:before:bg-white relative inline-block">
-                    <span class="relative text-white hover:text-pink-500">Sixteen</span>
-                </span></Link>
+                    <Link href="/" className="font-extrabold text-lg basis-0 flex items-center justify-center "><span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 duration-500 hover:before:bg-white relative inline-block">
+                        <span className="relative text-white duration-500 hover:text-pink-500">Sixteen</span>
+                    </span></Link>
                 </div>
-                <ul className="hidden md:static absolute md:flex flex-grow justify-center basis-0 text-sm [&>li>a]:inline-block [&>li>a]:p-3 [&>li>a]:text-black">
+                <ul className="z-[-1] md:z-auto md:static absolute bg-white w-full left-0 py-4  pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500 md:flex md:flex-grow md:justify-center basis-0 md:text-sm md:[&>li>a]:inline-block md:[&>li>a]:p-3 md:[&>li>a]:text-black">
                     <li className=' hover:border-zinc-600 hover:bg-slate-100 hover:shadow-xl active:shadow-2xl'><Link href="/about-us">Catalogos</Link></li>
                     <li className=' hover:border-zinc-600 hover:bg-slate-100 hover:shadow-xl active:shadow-2xl'><Link href="/ofertas">Ofertas</Link></li>
                     <li className=' hover:border-zinc-600 hover:bg-slate-100 hover:shadow-xl active:shadow-2xl'><Link href="/about-us">Preguntas&nbsp;Frecuentes</Link></li>
@@ -39,13 +36,40 @@ export default function NavBar() {
                     <li className='hover:rounded hover:border-zinc-600 hover:bg-slate-100 hover:shadow-xl active:shadow-2xl'><Link href="/"><Carrito className="w-5 h-5" /></Link></li>
                     <li className='hover:rounded hover:border-zinc-600 hover:bg-slate-100 hover:shadow-xl active:shadow-2xl'><Link href="/"><UserSVG className="w-5 h-5" /></Link></li>
                 </ul>
-                <ul className={`md:hidden basis-0 flex justify-center content-center cursor-pointer p-3 hover:rounded hover:border-zinc-600 hover:bg-slate-100 hover:shadow-xl active:shadow-2xl ${isOpenMenu ? 'activeMenuHamburg' : ''} hover:text-red-800`}>
-                    <button onClick={toggleMenuHamburg}>
+                <ul className={`md:hidden basis-0 flex justify-center content-center cursor-pointer p-3 hover:rounded hover:border-zinc-600 hover:bg-slate-100 hover:shadow-xl active:shadow-2xl ${isOpenMenu ? 'activeMenuHamburg top-[80px] opacity-100' : ''} hover:text-red-800`}>
+                    <span onClick={() => {setIsOpenMenu(!isOpenMenu)}}>
                         {
                             isOpenMenu ? <LogoExit className="w-5 h-5 text-black hover:text-red-800" /> : <LogoMenuHamburg className="w-5 h-5" />
                         }
-                    </button>
+                    </span>
                 </ul>
+                <div
+                    className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${isOpenMenu ? 'p-12 md:p-0 block' : 'hidden'
+                        }`}
+                >
+                    <ul className="h-screen md:h-auto items-center justify-center md:flex ">
+                        <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                            <Link href="#about" onClick={() => {setIsOpenMenu(!isOpenMenu)}}>
+                                About
+                            </Link>
+                        </li>
+                        <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                            <Link href="#blog" onClick={() => {setIsOpenMenu(!isOpenMenu)}}>
+                                Blogs
+                            </Link>
+                        </li>
+                        <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                            <Link href="#contact" onClick={() => {setIsOpenMenu(!isOpenMenu)}}>
+                                Contact
+                            </Link>
+                        </li>
+                        <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                            <Link href="#projects" onClick={() => {setIsOpenMenu(!isOpenMenu)}}>
+                                Projects
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </header>
     )
