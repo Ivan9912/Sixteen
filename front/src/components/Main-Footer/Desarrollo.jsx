16fedeac67e1c1de8478e5c0b1160b4b352c5414
Maidana Ivan <ivan.maidana99122@gmail.com>
2023-10-30 20:54:49 -0300

    first commit

--- front/src/components/Main-Footer/Desarrollo.jsx
+++ front/src/components/Main-Footer/Desarrollo.jsx
@@ -4,7 +4,7 @@ export default function Desarrollo() {
     return  <div className="h-10 w-full text-slate-50 bg-stone-700 py-4 px-10 flex items-center justify-center md:justify-between align-middle">
                 <ul className="flex flex-grow text-sm justify-center md:justify-center ">
                     <li>© 2023 SIXTEEN · All Rights Reserved.</li>
-                    <li className="hidden md:flex flex-grow justify-end"><span className="select-none hover:underline">Desarrollado por </span><Link href={"https://www.google.com"} className="hover:text-slate-400"> CodeCraze</Link></li>
+                    <li className="hidden md:flex flex-grow justify-end"><span className="select-none hover:underline">Desarrollado por</span><Link href={"https://www.google.com"} className="hover:text-slate-400">&nbsp;CodeCraze</Link></li>
                 </ul>
             </div>
 };
