a6d1d60ef0fe91915f77ae6692785bcbd462e4f8
Maidana Ivan <ivan.maidana99122@gmail.com>
2023-10-31 02:47:46 -0300

    first commit

--- front/src/app/home/page.jsx
+++ front/src/app/home/page.jsx
@@ -5,9 +5,9 @@ export default function Home() {
     return (
         <main className="flex text-black flex-col items-center justify-center h-screen select-none">
             <blockquote className="text-2xl font-semibold italic text-center text-slate-900">
-                Bienvenidos a&nbsp;
-                <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:-z-10 before:bg-pink-500 relative inline-block">
-                    <span className="relative text-white underline underline-offset-4 -z-[9]">Sixteen</span>
+                Bienvenidos a&nbsp;&nbsp;&nbsp;
+                <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 duration-500 hover:before:bg-white relative inline-block [&_span]:relative [&_span]:text-white [&:hover_span]:text-pink-500 [&:hover_span]:underline [&:hover_span]:underline-offset-4 [&_span]:duration-500 [&_span]:italic [&_span]:text-xl">
+                    <span>Sixteen</span>
                 </span>
             </blockquote>
         </main>
