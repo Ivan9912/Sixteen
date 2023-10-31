2f9671ac325867338ef91c5a050737e6afb7dc96
Maidana Ivan <ivan.maidana99122@gmail.com>
2023-10-31 00:29:44 -0300

    first commit

--- front/src/components/Main-Nav/Title.jsx
+++ front/src/components/Main-Nav/Title.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Title() {
     return  <div className='flex flex-grow basis-0 justify-start md:justify-items-start select-none'>
-                <Link href="/" className="font-extrabold text-lg basis-0 flex items-center justify-center "><span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 duration-500 hover:before:bg-white relative inline-block">
+                <Link href="/" title='Home' className="font-extrabold text-lg basis-0 flex items-center justify-center "><span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 duration-500 hover:before:bg-white relative inline-block">
                     <span className="relative text-white duration-500 hover:text-pink-500 hover:underline hover:underline-offset-4"><i><b>Sixteen</b></i></span>
                 </span></Link>
             </div>
--- front/src/components/Main-Nav/list/ListSvg.jsx
+++ front/src/components/Main-Nav/list/ListSvg.jsx
@@ -7,12 +7,12 @@ export default function ListSvg () {
     return  <ul className="hidden md:static absolute md:flex flex-grow justify-end basis-0 text-sm [&>li>a]:inline-block [&>li>a]:p-3 [&>li>a]:text-black [&>li]:gap-2">
                 <SearchBar />
                 <li className='hover:box-content hover:border hover:border-pink-500/20 hover:rounded hover:bg-pink-500/10 hover:shadow-xl active:shadow-2xl'>
-                    <Link href="/">
+                    <Link href="/cart" title="Carrito">
                         <Carrito className="w-5 h-5" />
                     </Link>
                 </li>
                 <li className='hover:box-content hover:border hover:border-pink-500/20 hover:rounded hover:bg-pink-500/10 hover:shadow-xl active:shadow-2xl'>
-                    <Link href="/">
+                    <Link href="/admin" title="Admin">
                         <UserSVG className="w-5 h-5" />
                     </Link>
                 </li>
