683d0c46b1cb7be2b1713bb74b20e95e628d157e
Maidana Ivan <ivan.maidana99122@gmail.com>
2023-11-01 18:56:43 -0300

    commit dropdown

--- front/src/components/Main-Nav/Menu/MenuHamburgButton.jsx
+++ front/src/components/Main-Nav/Menu/MenuHamburgButton.jsx
@@ -17,14 +17,27 @@ export default function MenuHamburgButton({ props }) {
             </ul>
             <nav className={`bg-transparent md:hidden w-screen fixed top-16 z-auto flex justify-center ${isOpenMenu ? '' : 'hidden'}`}>
                 <div className={`w-72 h-72 bg-white/90 justify-center flex border-b-2 border-pink-500`}>
-                    <ul className="flex flex-col w-full items-center z-auto justify-center fixed top-16 [&>li]:text-black [&>li]:text-base [&>li]:py-2 [&>li]:mx-6 [&>li]:text-center nth-2:mt-6 nth-1:hidden">
+                    <ul className="flex flex-col w-full items-center z-auto justify-center fixed top-16 [&>li]:text-black [&>li]:text-base [&>li]:py-2 [&>li]:px-6 [&>li]:text-center nth-2:pt-6 nth-1:hidden">
                         {
                             props.map((categorias, i) => {
-                                return  <li key={`$ID-${i}-${categorias.name}`} className="w-72 [&_a]:inline-block [&_a]:w-full hover:border-b-2 hover:border-pink-500  hover:text-pink-500 hover:bg-gray-100" onClick={() => { setIsOpenMenu(!isOpenMenu) }}>
-                                            <Link href={categorias.linknav}>
-                                                {categorias.name}
-                                            </Link>
-                                        </li>
+                                return <li key={`$ID-${i}-${categorias.name}`} className="w-72 [&_a]:inline-block [&_a]:w-full border-b-2 border-transparent active:border-b-2 active:border-pink-500  active:text-pink-500 active:bg-gray-100 hover:border-b-2 hover:border-pink-500  hover:text-pink-500 hover:bg-gray-100 group/List_H active:flex cursor-pointer" onClick={() => { setIsOpenMenu(!isOpenMenu) }}>
+                                    {
+                                        categorias.name == 'Catálogo'
+                                            ?
+                                            (<span className='w-full group/List_H active:flex'>
+                                                {categorias.name}                                           
+                                                {
+                                                    <ul className='relative bg-black text-white hidden group-active/List_H:flex'>
+                                                        <li>Item 1</li>
+                                                        <li>Item 2</li>
+                                                        <li>Item 3</li>
+                                                    </ul>
+                                                }
+                                            </span>)
+                                            :
+                                            (<Link href={categorias.linknav}>{categorias.name}</Link>)
+                                    }
+                                </li>
                             })
                         }
                     </ul>
