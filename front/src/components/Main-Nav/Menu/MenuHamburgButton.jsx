b2956464bbcb48a40da7b3f351d4735133cbd40b
Maidana Ivan <ivan.maidana99122@gmail.com>
2023-11-02 00:08:59 -0300

    commit dropdown

--- front/src/components/Main-Nav/Menu/MenuHamburgButton.jsx
+++ front/src/components/Main-Nav/Menu/MenuHamburgButton.jsx
@@ -1,7 +1,7 @@
 "use client";
 import LogoMenuHamburg from '../../../../public/img_svg/menu.jsx';
 import LogoExit from '../../../../public/img_svg/close.jsx';
-import Link from 'next/link.js';
+
 import { useState } from 'react';
 import SubList1 from './SubCategories/List.jsx';
 
@@ -19,19 +19,8 @@ export default function MenuHamburgButton({ props }) {
             <nav className={`bg-blaborder-black/0 md:hidden w-screen fixed top-16 z-auto flex justify-center ${isOpenMenu ? '' : 'hidden'}`}>
                 <div className={`w-72 h-auto bg-white/90 justify-center flex border-b-4 border-pink-500`}>
                     <ul className="flex flex-col w-full items-center z-auto justify-center top-16 [&>li]:text-black [&>li]:text-base [&>li]:py-2 [&>li]:px-6 [&>li]:text-center nth-7:mb-6 nth-2:mt-6 nth-1:hidden">
-                        {
-                            props.map((categorias, i) => {
-                                return <li key={`$ID-${i}-${categorias.name}`} className="w-72 [&_a]:inline-block [&_a]:w-full border-b-2 border-black/0 hover:border-b-2 hover:border-pink-500 hover:text-pink-500 hover:bg-gray-100 cursor-pointer group/sub_list_1">
-                                    {
-                                        categorias.name == 'Catálogo'
-                                            ?
-                                            (<SubList1 props={categorias} /> )
-                                            :
-                                            (<Link onClick={() => { setIsOpenMenu(!isOpenMenu) }} href={categorias.linknav}>{categorias.name}</Link>)
-                                    }
-                                </li>
-                            })
-                        }
+                        <SubList1 props={props} />
+                       
                     </ul>
                 </div>
             </nav>
--- front/src/components/Main-Nav/Menu/SubCategories/List.jsx
+++ front/src/components/Main-Nav/Menu/SubCategories/List.jsx
@@ -1,22 +1,35 @@
 import { useState } from 'react';
+import Link from 'next/link.js';
 
 export default function SubList1({ props }) {
     const [isOpenSubMenu, setIsOpenSubMenu] = useState(false);
-    const { list } = props;
-    return (
-        <span onClick={() => { setIsOpenSubMenu(!isOpenSubMenu) }}>
-            {props.name}
-            {
-                <ul className={`mt-2 bg-gray-100/90 text-black/90 flex-col text-start [&>li]:px-3 [&>li]:py-2 [&>li]:w-full w-full ${isOpenSubMenu ? '' : 'hidden'}`}>
+    return <>
+        {
+            props.map((categorias, i) => {
+                return <li key={`$ID-${i}-${categorias.name}`} className="w-72 [&_a]:inline-block [&_a]:w-full border-b-2 border-black/0 hover:border-b-2 hover:border-pink-500 hover:text-pink-500 hover:bg-gray-100 cursor-pointer group/sub_list_1">
                     {
-                        list.map((sub_1, i) => {
-                            return (
-                                <li key={`${sub_1}+-${i}`} className='text-xs border-b-2 border-black/0 hover:border-b-2 hover:border-pink-500 hover:text-pink-500 bg-gray-200 cursor-pointer group-hover/sub_list_1:bg-gray-300/90'>{sub_1.name}</li>
-                            )
-                        })
+                        categorias.name == 'Catálogo'
+                            ?
+                            (<span onClick={() => { setIsOpenSubMenu(!isOpenSubMenu) }}>
+                                {categorias.name}
+                                {
+                                    <ul className={`mt-2 bg-gray-100/90 text-black/90 flex-col text-start [&>li]:px-3 [&>li]:py-2 [&>li]:w-full w-full ${isOpenSubMenu ? '' : 'hidden'}`}>
+                                        {
+                                            categorias.list.map((sub_1, i) => {
+                                                return (
+                                                    <li key={`${sub_1}+-${i}`} className='text-xs border-b-2 border-black/0 hover:border-b-2 hover:border-pink-500 hover:text-pink-500 bg-gray-200 cursor-pointer group-hover/sub_list_1:bg-gray-300/90'>   
+                                                        {sub_1.name}
+                                                    </li>
+                                                )
+                                            })
+                                        }
+                                    </ul>
+                                }
+                            </span>)
+                            :
+                            (<Link href={categorias.linknav}>{categorias.name}</Link>)
                     }
-                </ul>
-            }
-        </span>
-    )
+                </li>
+            })}
+    </>
 };
