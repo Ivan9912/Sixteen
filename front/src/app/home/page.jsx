22464357705ccc493fbbacb3e85828bad2f93efc
Maidana Ivan <ivan.maidana99122@gmail.com>
2023-11-12 09:03:43 -0300

    commit

--- front/src/app/home/page.jsx
+++ front/src/app/home/page.jsx
@@ -74,28 +74,28 @@ export default function Home() {
             </div>
             {/* ------------------------Desde aca estan las cards de categorias de los 3 ------------------------------------------*/}
             <div div className=" grid grid-cols-1 lg:grid-cols-3 h-12/12 w-full justify-items-center lg:m-10 m-4 bg-black/5 lg:[&>div]:my-10 [&>div]:my-5 2xl:px-80 lg:px-16">
-                <div className="lg:h-80 lg:w-72 h-56 w-64 bg-pink-600 rounded border border-black/50 flex flex-col items-center">
+                <div className="lg:h-80 lg:w-72 h-56 w-64 bg-pink-600 rounded-lg border border-black/50 flex flex-col items-center hover:transition-transform hover:scale-105 hover:delay-75">
                     <div className="lg:h-64 lg:w-72 h-44 w-64 flex justify-center relative">
-                        <Image alt={"01"} src={'https://i.imgur.com/IglFLkP.png'} layout="cover" width={286} height={286} />
+                        <Image alt={"01"} src={'https://i.imgur.com/IglFLkP.png'} layout="cover" width={286} height={286} className="rounded-t-lg"/>
                     </div>
                     <div className="h-full w-full flex justify-center">
-                        <Link className="w-full text-center text-xs font-semibold lg:text-sm lg:font-bold py-5 inline-block" href={'/'}>VESTIDO DE MORLEY RAYADO</Link>
+                        <Link className="w-full text-center text-white text-xs font-semibold lg:text-sm lg:font-bold py-5 inline-block" href={'/'}>VESTIDO DE MORLEY RAYADO</Link>
                     </div>
                 </div>
-                <div className="lg:h-80 lg:w-72 h-56 w-64 bg-pink-600 rounded border border-black/50 flex flex-col items-center">
+                <div className="lg:h-80 lg:w-72 h-56 w-64 bg-pink-600 rounded-lg border border-black/50 flex flex-col items-center hover:transition-transform hover:scale-105 hover:delay-75">
                     <div className="lg:h-64 lg:w-72 h-44 w-64 flex justify-center relative">
-                        <Image alt={"01"} src={'https://i.imgur.com/mrOkLUB.png'} layout="cover" width={286} height={286} />
+                        <Image alt={"01"} src={'https://i.imgur.com/mrOkLUB.png'} layout="cover" width={286} height={286} className="rounded-t-lg"/>
                     </div>
                     <div className="h-full w-full flex justify-center">
-                        <Link className="w-full text-center text-xs font-semibold lg:text-sm lg:font-bold py-5 inline-block" href={'/'}>VESTIDO RAYADO CON TAJO</Link>
+                        <Link className="w-full text-center text-white text-xs font-semibold lg:text-sm lg:font-bold py-5 inline-block" href={'/'}>VESTIDO RAYADO CON TAJO</Link>
                     </div>
                 </div>
-                <div className="lg:h-80 lg:w-72 h-56 w-64 bg-pink-600 rounded border border-black/50 flex flex-col items-center">
-                    <div className="lg:h-64 lg:w-72 h-44 w-64 flex justify-center relative">
-                        <Image alt={"01"} src={'https://i.imgur.com/SpPNf9T.png'} layout="cover" width={286} height={286} />
+                <div className="lg:h-80 lg:w-72 h-56 w-64 bg-pink-600 rounded-lg border border-black/50 flex flex-col items-center hover:transition-transform hover:scale-105 hover:delay-75">
+                    <div className="lg:h-64 lg:w-72 h-44 w-64 flex justify-center relative bg-center">
+                        <Image alt={"01"} src={'https://i.imgur.com/SpPNf9T.png'} layout="cover" width={286} height={286} className="rounded-t-lg"/>
                     </div>
                     <div className="h-full w-full flex justify-center">
-                        <Link className="w-full text-center text-xs font-semibold lg:text-sm lg:font-bold py-5 inline-block" href={'/'}>LONAS</Link>
+                        <Link className="w-full text-center text-white text-xs font-semibold lg:text-sm lg:font-bold py-5 inline-block" href={'/'}>LONAS</Link>
                     </div>
                 </div>
 
