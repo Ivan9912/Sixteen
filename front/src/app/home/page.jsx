68f350fbc381e4b60b040901bdc9427cb2c3c8de
Maidana Ivan <ivan.maidana99122@gmail.com>
2023-11-12 09:07:35 -0300

    commit

--- front/src/app/home/page.jsx
+++ front/src/app/home/page.jsx
@@ -7,9 +7,9 @@ metadata.title = 'Sixteen | Home';
 export default function Home() {
     return (
         <main className="flex text-black flex-col items-center justify-center h-full select-none mt-16 lg:mt-28">
-            <blockquote className="text-2xl font-semibold italic text-center text-slate-900 my-8 z-[2]">
+            <blockquote className="lg:text-2xl font-semibold italic text-center text-slate-900 my-8 z-[2]">
                 Bienvenidos a&nbsp;&nbsp;&nbsp;
-                <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:z-0 before:bg-pink-500 duration-500 hover:before:bg-[#f5f5f5] relative inline-block [&_span]:relative [&_span]:text-[#f5f5f5] [&:hover_span]:text-pink-500 [&:hover_span]:underline [&:hover_span]:underline-offset-4 [&_span]:duration-500 [&_span]:italic [&_span]:text-xl [&_span]:z-[1]">
+                <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:z-0 before:bg-pink-500 duration-500 hover:before:bg-[#f5f5f5] relative inline-block [&_span]:relative [&_span]:text-[#f5f5f5] [&:hover_span]:text-pink-500 [&:hover_span]:underline [&:hover_span]:underline-offset-4 [&_span]:duration-500 [&_span]:italic [&_span]:lg:text-xl [&_span]:z-[1]">
                     <span>Sixteen</span>
                 </span>
             </blockquote>
@@ -98,10 +98,9 @@ export default function Home() {
                         <Link className="w-full text-center text-white text-xs font-semibold lg:text-sm lg:font-bold py-5 inline-block" href={'/'}>LONAS</Link>
                     </div>
                 </div>
-
             </div>
             <div>
-                <h1 className="text-[3rem]">PRODUCTOS DESTACADOS</h1>
+                <h1 className="lg:text-[3rem] text-lg my-4">PRODUCTOS DESTACADOS</h1>
             </div>
         </main>
     )
